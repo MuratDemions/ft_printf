2c756825b16034988cfa350f3bc068b46f437f32
Murat Sipit <musipit@k2m02s06.42kocaeli.com.tr>
2026-01-28 19:54:11 +0300

    ADD: libforprintf_v0.1

---
+++ libforprintf.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libforprintf.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: musipit <musipit@student.42kocaeli.com.tr> #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-01-28 16:37:40 by musipit           #+#    #+#             */
+/*   Updated: 2026-01-28 16:37:40 by musipit          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFORPRINTFT_H
+# define LIBFORPRINTF_H
+
+void    ft_putchar(char c);
+void    ft_putstr(char *s);
+
+#endif
