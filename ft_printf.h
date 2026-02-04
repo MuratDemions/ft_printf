9ea232a58a4867a47a7d79ded7259683016c9444
Murat Sipit <musipit@k2m08s03.42kocaeli.com.tr>
2026-02-04 17:42:30 +0300

    FIX: printf.h    RM: writer

--- ft_printf.h
+++ ft_printf.h
@@ -13,8 +13,7 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include <stdarg.h>
-
+int	ft_printf(const char *format, ...);
 int	ft_putchar(char c);
 int	ft_putstr(char *s);
 int	ft_hex(unsigned long long number, int format);
